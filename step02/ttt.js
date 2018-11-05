2ddb4cf225a0404bcedc0ec8043cf43cec998319
Michael Gagnon <mikegagnon@gmail.com>
2018-11-04 20:55:16 -0800

    step02

---
+++ step02/ttt.js
@@ -0,0 +1,21 @@
+var PLAYER = "X";
+var CELL_MARK = new Array(9);
+
+function cellClick() {
+    var cellNumber = $(this).data("cell-number");
+
+    if (CELL_MARK[cellNumber] == "X" || CELL_MARK[cellNumber] == "O") {
+        return;
+    }
+
+    CELL_MARK[cellNumber] = PLAYER;
+    $(this).text(PLAYER);
+
+    if (PLAYER == "X") {
+        PLAYER = "O";
+    } else {
+        PLAYER = "X";
+    }
+}
+
+$(".cell").click(cellClick);
